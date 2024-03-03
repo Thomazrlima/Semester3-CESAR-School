2093a18f2a6b293e9a7fb868b337bcef95f95ad1
Thomazrlima <thomazlima211@gmail.com>
2024-03-03 18:19:08 -0300

    Outra correção da lista extra 1

--- POO/JavaEstruturado/src/JavaEstruturado/JavaEstruturado3.java
+++ POO/JavaEstruturado/src/JavaEstruturado/JavaEstruturado3.java
@@ -14,8 +14,10 @@ public class JavaEstruturado3 {
         float x5 = ENTRADA.nextInt();
 
         float soma = x1 + x2 + x3 + x4 + x5;
+        float prod = x1*x2*x3*x4*x5;
+
         float x = (soma)/5;
-        double xg = Math.pow(soma, 1/5);
+        float xg = (float) Math.pow(prod, 1.0/5);
         float mh = (5/((1/x1) + (1/x2) + (1/x3) + (1/x4) + (1/x5)));
 
         System.out.println(x);
