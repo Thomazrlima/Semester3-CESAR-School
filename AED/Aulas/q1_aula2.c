770c4c00df7e2b9c17816e801237b5f86b6a7852
Thomazrlima <thomazlima211@gmail.com>
2024-03-05 22:12:56 -0300

    finalização da q1 aula 2

--- AED/Aulas/q1_aula2.c
+++ AED/Aulas/q1_aula2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct{
   char nome[52];
@@ -9,7 +10,7 @@ typedef struct{
 
 void adicionar(Node **head, Node **tail, char *n);
 void deletar(Node **head, Node **tail);
-void consultar(Node **head);
+void consultar(Node **head, char *comparado);
 void Caminho(int escolha, Node **head, Node **tail);
 int Menu(void);
 
@@ -53,26 +54,31 @@ void deletar(Node **head, Node **tail) {
   }
 }
 
-void consultar(Node **head) {
+void consultar(Node **head, char *comparado) {
     Node *aux = *head;
-    int count = 1;
+    int count = 0;
 
-    if(aux == NULL) {
-        printf("Lista vazia\n");
+    if (aux == NULL) {
+        printf("\nLista vazia\n");
         return;
     }
 
-    printf("Lista de pacientes:\n");
     while (aux != NULL) {
-        printf("%d. %s\n", count, aux->nome);
+        count += 1;
+        if (strcmp(aux->nome, comparado) == 0) {
+            printf("\nEncontrado na posição %d\n", count);
+            return;
+        }
         aux = aux->prox;
-        count++;
     }
+
+    printf("\nPaciente não encontrado :(\n");
 }
+
 
 int Menu(void) {
     int escolha;
-    printf("O que você deseja fazer?\n");
+    printf("\nO que você deseja fazer?\n");
     printf("1 - Incluir paciente\n");
     printf("2 - Realizar atendimento do paciente\n");
     printf("3 - Consultar a posição atual do paciente pelo nome\n");
@@ -85,32 +91,57 @@ int Menu(void) {
 
 void Caminho(int escolha, Node **head, Node **tail) {
     Node paciente;
+    static int atendidos = 0;
+    char comparado[52];
+  
     switch (escolha) {
+      
         case 1:
-          
+
+            system("clear");
             printf("\nDigite o nome do paciente: ");
             scanf("%s", paciente.nome);
             adicionar(head, tail, paciente.nome);
             break;
       
         case 2:
-            
-            printf("\nDigite o nome do paciente: ");
-            scanf("%s", paciente.nome);
+
+            system("clear");
+            printf("\nO Paciente a ser atendido é: %s\n", (*head)->nome);
             deletar(head, tail);
+            atendidos += 1;
             break;
       
         case 3:
-            printf("\nSeus pacientes:\n: ");
-            consultar(head);
+
+            system("clear");
+            printf("\nDigite o nome do paciente: ");
+            scanf("%s", comparado);
+            consultar(head, comparado);
             break;
+
+      
         case 4:
-            
+
+            system("clear");
+            printf("\nPacientes atendidos: %d\n", atendidos);
             break;
+      
         case 5:
-            printf("Tchau!");
-            exit(0);;
+
+            system("clear");
+            printf("Finalizando o programa \n");
+            for(int i = 0; i < 5; i++){
+              sleep(1);
+              printf(".\n");
+            }
+            printf("Tchau!\n");
+            exit(0);
+      
         default:
+          
+            system("clear");
             printf("Opção inválida.\n");
+      
     }
 }
