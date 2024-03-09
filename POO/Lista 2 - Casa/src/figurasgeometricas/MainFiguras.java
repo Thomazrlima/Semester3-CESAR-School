fb9fb1e4dba29e11c618986f2c27f57b8fcdbbe0
Thomazrlima <thomazlima211@gmail.com>
2024-03-09 19:04:44 -0300

    q2 lista 2 poo

--- POO/Lista 2 - Casa/src/figurasgeometricas/MainFiguras.java
+++ POO/Lista 2 - Casa/src/figurasgeometricas/MainFiguras.java
@@ -8,7 +8,7 @@ public class MainFiguras {
 
         System.out.println("Informe a forma do terreno:");
         System.out.println("1- Triangulo");
-        System.out.println("2- Retangulo");
+        System.out.println("2- Paralelogramo");
         System.out.println("3- Circulo");
 
         int forma = scanner.nextInt();
@@ -20,29 +20,58 @@ public class MainFiguras {
 
         double area = 0;
         switch (forma) {
+
             case 1:
+
                 System.out.println("Informe os lados do triângulo:");
                 double l1 = scanner.nextDouble();
                 double l2 = scanner.nextDouble();
                 double l3 = scanner.nextDouble();
                 Triangulo triangulo = new Triangulo(l1, l2, l3);
                 area = triangulo.calcularArea();
+
                 break;
             case 2:
-                System.out.println("Informe os lados do retângulo ou quadrado:");
+
+                System.out.println("Informe os lados do Paralelogramo:");
                 double lado = scanner.nextDouble();
-                area = Math.pow(lado, 2); // Área do quadrado
+                System.out.println("Informe o lado do Paralelogramo:");
+                double ladoAdjacente = scanner.nextDouble();
+                System.out.println("Informe o angulo do Paralelogramo:");
+                double angulo = scanner.nextDouble();
+                Paralelogramo paralelogramo = new Paralelogramo(lado,ladoAdjacente, angulo);
+                area = paralelogramo.calcularArea();
                 break;
+
             case 3:
+
                 System.out.println("Informe o raio do círculo:");
                 double raio = scanner.nextDouble();
                 Circulo circulo = new Circulo(raio);
                 area = circulo.calcularArea();
                 break;
+
         }
 
-        double valorPorMetroQuadrado =
+        System.out.println("Informe o cep:");
+        scanner.nextLine();
+        int cep = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.println("Informe o logradouro:");
+        String logradouro = scanner.nextLine();
+
+        System.out.println("Informe o numero:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Informe complemento:");
+        String complemento = scanner.nextLine();
+
+        System.out.println("Informe o valor por metro quadrado:");
+        double valorPorMetroQuadrado = scanner.nextDouble();
+
+        Endereco endereco = new Endereco(cep, logradouro, numero, complemento);
         Terreno terreno = new Terreno(valorPorMetroQuadrado, endereco);
         double valor = terreno.calcularValorTerreno(area, valorPorMetroQuadrado);
 
