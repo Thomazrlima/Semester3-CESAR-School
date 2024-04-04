6aae6d2fcd54cae4526eee00fdec6612199d0272
Thomazrlima <thomazlima211@gmail.com>
2024-04-03 21:27:07 -0300

    implementação V1

--- SO/Implementações/AV1/Implementação.c
+++ SO/Implementações/AV1/Implementação.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 struct Tarefa {
-    char nome[3];
-    int periodo;
-    int cpub;
-    int resto;
-    int completadas;
-    int perdido;
+  char nome[3];
+  int periodo;
+  int cpub;
+  int resto;
+  int perdido;
+  int completo;
+  int morto;
+  int prioridade;
 };
 
 void rate(struct Tarefa tarefas[], int qtd, int total);
-void iniciarTarefas(struct Tarefa tarefas[], int periodos[], int execucoes[], int qtd); // Provisório
+void iniciarTarefas(struct Tarefa tarefas[], int periodos[], int execucoes[],
+                    int qtd);
 
 int main(void) {
   struct Tarefa tarefas[2];
@@ -27,90 +30,69 @@ int main(void) {
 
 void rate(struct Tarefa tarefas[], int qtd, int total) {
   int atual = 0;
-  int i = 0;
+  int i, j;
+  struct Tarefa temp;
+
+  while (atual < total) {
+
+    for (i = 0; i < qtd - 1; i++) {
+      if (tarefas[i].prioridade < tarefas[i + 1].prioridade &&
+          tarefas[i].resto != 0) {
+
+        if (tarefas[i].resto > 0) {
+          printf("Executando tarefa %s\n", tarefas[i].nome);
+          tarefas[i].resto--;
+          if (tarefas[i].resto == 0) {
+            tarefas[i].completo = 1;
+          }
+        }
+      }
+
+      if (atual % tarefas[i].periodo == 0) {
+        if (tarefas[i].resto != 0) {
+          tarefas[i].resto = tarefas[i].cpub;
+          tarefas[i].morto += 1;
+          tarefas[i].prioridade = 0;
+        } else {
+          tarefas[i].resto = tarefas[i].cpub;
+          tarefas[i].prioridade = tarefas[i].periodo;
+        }
+      }
+    }
+    atual++;
+  }
 
   printf("EXECUTION BY RATE\n");
 
-  while (atual < total) {
-    if (tarefas[i].resto > 0) {
-      if (tarefas[i].completadas == 0) {
-        
-        printf("[%s] for %d units - F\n", tarefas[i].nome, tarefas[i].cpub);
-        
-        tarefas[i].resto -= tarefas[i].cpub;
-        atual += tarefas[i].cpub;
-        tarefas[i].completadas++;
-        
-        if (tarefas[i].resto <= 0) {
-          tarefas[i].completadas = 1;
-        }
-        
-      } else if (tarefas[i].completadas == 1) {
-        
-        printf("[%s] for %d units - H\n", tarefas[i].nome, tarefas[i].cpub);
-        
-        tarefas[i].resto -= tarefas[i].cpub;
-        atual += tarefas[i].cpub;
-        tarefas[i].completadas++;
-        
-        if (tarefas[i].resto <= 0) {
-          tarefas[i].completadas = 2;
-        }
-        
-      } else if (tarefas[i].completadas == 2) {
-        
-        printf("[%s] for %d units - L\n", tarefas[i].nome, tarefas[i].resto);
-        
-        atual += tarefas[i].resto;
-        tarefas[i].completadas++;
-        tarefas[i].resto = 0;
-        
-      }
-    } else {
-      
-      i++;
-      atual++;
-      if (i == qtd) {
-        i = 0;
-      }
-      
-      if (i >= qtd) {
-        
-        printf("idle for %d units\n", total - atual);
-        atual = total;
-        
-      }
-    }
-  }
-
   printf("LOST DEADLINES\n");
   for (i = 0; i < qtd; i++) {
-    if (tarefas[i].completadas != 2) {
-      printf("[%s] %d\n", tarefas[i].nome, tarefas[i].completadas);
+    if (tarefas[i].completo != 2) {
+      printf("[%s] %d\n", tarefas[i].nome, tarefas[i].morto);
     }
   }
 
   printf("COMPLETE EXECUTION\n");
   for (i = 0; i < qtd; i++) {
-    printf("[%s] %d\n", tarefas[i].nome, tarefas[i].completadas);
+    printf("[%s] %d\n", tarefas[i].nome, tarefas[i].completo);
   }
 
   printf("KILLED\n");
   for (i = 0; i < qtd; i++) {
-    if (tarefas[i].completadas == 0) {
+    if (tarefas[i].completo == 0) {
       printf("[%s] 1\n", tarefas[i].nome);
     }
   }
 }
 
-void iniciarTarefas(struct Tarefa tarefas[], int periodos[], int execucoes[], int qtd) {
+void iniciarTarefas(struct Tarefa tarefas[], int periodos[], int execucoes[],
+                    int qtd) {
   for (int i = 0; i < qtd; i++) {
-    sprintf(tarefas[i].nome, "T%d", i + 1); // Provisório, vão entrar os nomes reais
+    sprintf(tarefas[i].nome, "T%d", i + 1);
     tarefas[i].periodo = periodos[i];
+    tarefas[i].prioridade = periodos[i];
     tarefas[i].cpub = execucoes[i];
     tarefas[i].resto = execucoes[i];
-    tarefas[i].completadas = 0;
-    tarefas[i].completadas = 0;
-    tarefas[i].perdido = 0;
+    tarefas[i].completo = 0;
+    tarefas[i].morto = 0;
   }
 }
