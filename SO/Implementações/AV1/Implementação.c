33c2c797a11713a459fef8b3e7ecee4b94ced35c
Thomazrlima <thomazlima211@gmail.com>
2024-04-04 18:07:05 -0300

    mexida na saída do print

--- SO/Implementações/AV1/Implementação.c
+++ SO/Implementações/AV1/Implementação.c
@@ -9,6 +9,7 @@ struct Tarefa {
   int completo;
   int morto;
   int prioridade;
+  int units;
 };
 
 void rate(struct Tarefa tarefas[], int qtd, int total);
@@ -29,36 +30,69 @@ int main(void) {
 }
 
 void rate(struct Tarefa tarefas[], int qtd, int total) {
-  int atual = 0;
+  int atual = 1, bou = 1, idle = 0;
   int i, j;
-  struct Tarefa temp;
 
-  while (atual < total) {
+  while (atual <= total) {
 
-    for (i = 0; i < qtd - 1; i++) {
-      if (tarefas[i].prioridade < tarefas[i + 1].prioridade &&
-          tarefas[i].resto != 0) {
+    int flag = 0;
 
-        if (tarefas[i].resto > 0) {
-          printf("Executando tarefa %s\n", tarefas[i].nome);
+    for (i = 0; i < qtd; i++) {
+      for (j = 0; j < qtd; j++) {
+
+        if (tarefas[i].prioridade < tarefas[j].prioridade &&
+            tarefas[i].resto != 0) {
+          // printf("Executando tarefa %s %d\n", tarefas[i].nome, atual); // Debug
           tarefas[i].resto--;
+          tarefas[i].units++;
+          flag = 1;
+
           if (tarefas[i].resto == 0) {
-            tarefas[i].completo = 1;
+            tarefas[i].completo += 1;
+            tarefas[i].prioridade = 999;
+            printf("[%s] for %d units - F\n", tarefas[i].nome, tarefas[i].units);
+            tarefas[i].units = 0;
           }
         }
       }
+    }
 
+    if (!flag) {
+      printf("idle for %d units\n", idle);
+      idle++;
+    } else {
+      idle = 0;
+    }
+
+    for (i = 0; i < qtd; i++) {
       if (atual % tarefas[i].periodo == 0) {
+
         if (tarefas[i].resto != 0) {
+
+          printf("[%s] for %d units - L\n", tarefas[i].nome, tarefas[i].units);
           tarefas[i].resto = tarefas[i].cpub;
-          tarefas[i].morto += 1;
-          tarefas[i].prioridade = 0;
+          tarefas[i].perdido += 1;
+          tarefas[i].prioridade = tarefas[i].periodo;
+          tarefas[i].units = 0;
+          // printf("%d\n", tarefas[i].prioridade); // Debug
+
         } else {
+
           tarefas[i].resto = tarefas[i].cpub;
           tarefas[i].prioridade = tarefas[i].periodo;
+          // printf("%d\n", tarefas[i].prioridade); // Debug
         }
       }
     }
+
+    if (atual + 1 == total) {
+      for (i = 0; i < qtd; i++) {
+        if (tarefas[i].resto - 1 > 0) {
+          tarefas[i].morto++;
+        }
+      }
+    }
+
     atual++;
   }
 
@@ -66,9 +100,7 @@ void rate(struct Tarefa tarefas[], int qtd, int total) {
 
   printf("LOST DEADLINES\n");
   for (i = 0; i < qtd; i++) {
-    if (tarefas[i].completo != 2) {
-      printf("[%s] %d\n", tarefas[i].nome, tarefas[i].morto);
-    }
+    printf("[%s] %d\n", tarefas[i].nome, tarefas[i].perdido);
   }
 
   printf("COMPLETE EXECUTION\n");
@@ -78,9 +110,7 @@ void rate(struct Tarefa tarefas[], int qtd, int total) {
 
   printf("KILLED\n");
   for (i = 0; i < qtd; i++) {
-    if (tarefas[i].completo == 0) {
-      printf("[%s] 1\n", tarefas[i].nome);
-    }
+    printf("[%s] %d\n", tarefas[i].nome, tarefas[i].morto);
   }
 }
 
@@ -94,5 +124,7 @@ void iniciarTarefas(struct Tarefa tarefas[], int periodos[], int execucoes[],
     tarefas[i].resto = execucoes[i];
     tarefas[i].completo = 0;
     tarefas[i].morto = 0;
+    tarefas[i].units = 0;
+    tarefas[i].perdido = 0;
   }
 }
