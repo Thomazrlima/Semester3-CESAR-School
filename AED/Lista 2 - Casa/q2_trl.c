5e4e9c8dd16f710583e9add02696d0e180e67f5a
Thomazrlima <thomazlima211@gmail.com>
2024-04-27 18:37:02 -0300

    Reorganização do repositório

--- AED/Lista 2 - Casa/q2_trl.c
+++ AED/Lista 2 - Casa/q2_trl.c
@@ -1,140 +1,199 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node {
-    int valor;
-    struct Node *prox;
-    struct Node *ant;
+  char pais[50];
+  int O;
+  int P;
+  int B;
+  struct Node *prox;
 } Node;
 
-void inserir(Node **head, Node **tail, int valor);
-void printLista(Node *head, Node *tail);
-void remover(Node **head, Node **tail, Node **removido, int flag);
-Node *k(Node **head, int k);
-Node *m(Node **tail, int m);
+void inserir(Node **head, char pais[], int O, int P, int B);
+void imprimir(Node *head);
+void ouro(Node **head);
+void prata(Node **head);
+void bronze(Node **head);
+void pais(Node **head);
+void trocar(Node **a, Node **b);
 
 int main(void) {
-    int N, k_value, m_value, i;
-    Node *head = NULL;
-    Node *tail = NULL;
-    Node *pontk = NULL;
-    Node *pontm = NULL;
+  Node *head = NULL;
+  char nacao[50];
+  int O, P, B, N;
 
-    while (1) {
-        scanf("%d %d %d", &N, &k_value, &m_value);
+  scanf("%d", &N);
 
-        if (N == 0 && k_value == 0 && m_value == 0) {
-            break;
-        }
+  for (int i = 0; i < N; i++) {
+    scanf("%s %d %d %d", nacao, &O, &P, &B);
+    inserir(&head, nacao, O, P, B);
+  }
 
-        head = tail = NULL;
+  // pais(&head);
+  // bronze(&head);
+  prata(&head);
+  ouro(&head);
 
-        for (i = 0; i < N; i++) {
-            inserir(&head, &tail, i + 1);
-        }
+  imprimir(head);
 
-        while (head != NULL) {
-            pontk = k(&head, k_value);
-            pontm = m(&tail, m_value);
-
-            if(pontk->valor == pontm->valor) {
-                remover(&head, &tail, &pontk, 1);
-            }else {
-                remover(&head, &tail, &pontk, 1);
-                remover(&head, &tail, &pontm, 0);
-            }
-        }
-    }
-
-    return 0;
+  return 0;
 }
 
-void inserir(Node **head, Node **tail, int valor) {
-    Node *novo = (Node *)malloc(sizeof(Node));
-    novo->valor = valor;
+void inserir(Node **head, char pais[], int O, int P, int B) {
+  Node *novo = (Node *)malloc(sizeof(Node));
+  if (novo == NULL) {
+    return;
+  }
 
-    if (*head == NULL && *tail == NULL) {
-        *head = *tail = novo;
-        novo->ant = novo->prox = novo;
-    } else {
-        novo->prox = NULL;
-        novo->ant = *tail;
-        (*tail)->prox = novo;
-        *tail = novo;
+  strcpy(novo->pais, pais);
+  novo->O = O;
+  novo->P = P;
+  novo->B = B;
+  novo->prox = NULL;
+
+  if (*head == NULL) {
+    *head = novo;
+  } else {
+    Node *atual = *head;
+    while (atual->prox != NULL) {
+      atual = atual->prox;
     }
-    (*tail)->prox = *head;
-    (*head)->ant = novo;
+    atual->prox = novo;
+  }
 }
 
-void printLista(Node *head, Node *tail) {
-    if (head == NULL) {
-        printf("Lista vazia.\n");
-        return;
+void ouro(Node **head) {
+  Node *atual = *head;
+  Node *max = NULL;
+  Node *velho = NULL;
+
+  while (atual != NULL) {
+    max = atual;
+    velho = NULL;
+    Node *temp = atual->prox;
+
+    while (temp != NULL) {
+      if (temp->O > max->O) {
+        max = temp;
+        velho = atual;
+      }
+      temp = temp->prox;
     }
 
-    Node *atual = head;
-    do {
-        printf("%d ", atual->valor);
-        atual = atual->prox;
-    } while (atual != tail->prox);
-    printf("\n");
+    if (max != atual) {
+      trocar(&atual, &max);
+      if (velho != NULL) {
+        trocar(&velho->prox, &max->prox);
+      }
+    }
+
+    atual = atual->prox;
+  }
 }
 
-Node *k(Node **head, int k) {
-    Node *pontk = (*head);
+void prata(Node **head) {
+  Node *atual = *head;
+  Node *max = NULL;
+  Node *velho = NULL;
 
-    for (int i = 1; i < k; i++) {
-        pontk = pontk->prox;
+  while (atual != NULL) {
+    max = atual;
+    velho = NULL;
+    Node *temp = atual->prox;
+
+    while (temp != NULL) {
+      if (temp->P > max->P) {
+        max = temp;
+        velho = atual;
+      }
+      temp = temp->prox;
     }
 
-    return pontk;
+    if (max != atual) {
+      trocar(&atual, &max);
+      if (velho != NULL) {
+        trocar(&velho->prox, &max->prox);
+      }
+    }
+
+    atual = atual->prox;
+  }
 }
 
-Node *m(Node **tail, int m) {
-    Node *pontm = (*tail);
+void bronze(Node **head) {
+  Node *atual = *head;
+  Node *max = NULL;
+  Node *velho = NULL;
 
-    for (int i = 1; i < m; i++) {
-        pontm = pontm->ant;
+  while (atual != NULL) {
+    max = atual;
+    velho = NULL;
+    Node *temp = atual->prox;
+
+    while (temp != NULL) {
+      if (temp->B > max->B) {
+        max = temp;
+        velho = atual;
+      }
+      temp = temp->prox;
     }
 
-    return pontm;
+    if (max != atual) {
+      trocar(&atual, &max);
+      if (velho != NULL) {
+        trocar(&velho->prox, &max->prox);
+      }
+    }
+
+    atual = atual->prox;
+  }
 }
 
-void remover(Node **head, Node **tail, Node **removido, int flag) {
-    Node *remover;
-    Node *pont = (*removido);
-    if(flag){
-        (*removido) = (*removido)->prox;
-    }else{
-        (*removido) = (*removido)->ant;
+void pais(Node **head) {
+  Node *atual = *head;
+  Node *max = NULL;
+  Node *velho = NULL;
+
+  while (atual != NULL) {
+    max = atual;
+    velho = NULL;
+    Node *temp = atual->prox;
+
+    while (temp != NULL) {
+      if (strcmp(temp->pais, max->pais) > 0) {
+        max = temp;
+        velho = atual;
+      }
+      temp = temp->prox;
     }
 
-
-    if (*head == *tail) {
-        *head = *tail = NULL;
-    } else if ((*tail)->valor == pont->valor) {
-        remover = *tail;
-        *tail = remover->ant;
-        remover->prox->ant = remover->ant;
-        remover->ant->prox = remover->prox;
-        printf("  %d ", remover->valor);
-        free(remover);
-
-    } else if ((*head)->valor == pont->valor) {
-        remover = *head;
-        *head = (*head)->prox;
-        (*head)->ant = *tail;
-        (*tail)->prox = *head;
-        printf("  %d ", remover->valor);
-        free(remover);
-    } else {
-        remover = *head;
-        while (remover != pont) {
-            remover = remover->prox;
-        }
-        remover->prox->ant = remover->ant;
-        remover->ant->prox = remover->prox;
-        printf("  %d ", remover->valor);
-        free(remover);
+    if (max != atual) {
+      trocar(&atual, &max);
+      if (velho != NULL) {
+        trocar(&velho->prox, &max->prox);
+      }
     }
+
+    atual = atual->prox;
+  }
+}
+
+void trocar(Node **a, Node **b) {
+  Node temp;
+
+  temp = **a;
+  **a = **b;
+  **b = temp;
+
+  temp.prox = (*a)->prox;
+  (*a)->prox = (*b)->prox;
+  (*b)->prox = temp.prox;
+}
+
+void imprimir(Node *head) {
+  while (head != NULL) {
+    printf("%s %d %d %d\n", head->pais, head->O, head->P, head->B);
+    head = head->prox;
+  }
 }
