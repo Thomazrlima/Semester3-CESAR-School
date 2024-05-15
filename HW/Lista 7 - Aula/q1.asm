556b4d4a83191fe14c1b0afee7024ce588c9af5a
thomazrlima <thomazlima211@gmail.com>
2024-05-15 09:52:53 -0300

    Lista 7 HW

---
+++ HW/Lista 7 - Aula/q1.asm
@@ -0,0 +1,12 @@
+.data
+	true: asciiz "true"
+	true: asciiz "false"
+.text
+	li $v0, 5
+	sycall
+	li $v0, 5
+	sycall
+	li $v0, 5
+	sycall
+	li $v0, 5
+	sycall
---
+++ HW/Lista 7 - Aula/q2.asm
@@ -0,0 +1,4 @@
+add $t0, $s0, $s3
+beq $t0, 7, true
+bne $s1, 2, false
+beq $s2, 5, false
